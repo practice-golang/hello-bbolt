170437c99452d8277abd42680be1f5395cca42cd
Practice Golang <practice.golang@gmail.com>
2024-07-27 16:02:33 +0900

    misc

--- main.go
+++ main.go
@@ -1,12 +1,95 @@
 package main // import "hello-bbolt"
 
 import (
+	"bytes"
+	"crypto/rand"
+	"errors"
 	"fmt"
 
 	"go.etcd.io/bbolt"
+	"golang.org/x/crypto/chacha20poly1305"
 )
 
-var xchacha *Xchacha
+var (
+	xchacha        *Xchacha
+	passphrase     = string("my passsphrase")
+	additionalData = []byte("padding data")
+)
+
+func SetupXchacha() (*Xchacha, error) {
+	var err error
+	var result Xchacha
+
+	switch {
+	case KeyExists("xchacha", []byte("data")):
+		err = db.View(func(tx *bbolt.Tx) error {
+			bucket := tx.Bucket([]byte("xchacha"))
+			if bucket == nil {
+				return nil
+			}
+
+			keyData := bucket.Get([]byte("data"))
+			if keyData == nil {
+				return nil
+			}
+
+			result, err = Deserialize(keyData)
+			if err != nil {
+				return fmt.Errorf("failed to deserialize: %v", err)
+			}
+
+			return nil
+		})
+
+		enteredKey := generateKeyFromString(passphrase)
+		if !bytes.Equal(enteredKey, result.Key) {
+			result = Xchacha{}
+			return nil, errors.New("wrong passphrase")
+		}
+
+		if err != nil {
+			return nil, err
+		}
+
+	default:
+		// result.Key = make([]byte, chacha20poly1305.KeySize)
+		// if _, err := rand.Read(result.Key); err != nil {
+		// 	return nil, fmt.Errorf("failed to generate key: %v", err)
+		// }
+		result.Key = generateKeyFromString(passphrase)
+		result.Nonce = make([]byte, chacha20poly1305.NonceSizeX)
+		if _, err := rand.Read(result.Nonce); err != nil {
+			return nil, fmt.Errorf("failed to generate nonce: %v", err)
+		}
+
+		xchachaBIN, err := Serialize(result)
+		if err != nil {
+			return nil, fmt.Errorf("failed to serialize: %v", err)
+		}
+
+		err = db.Update(func(tx *bbolt.Tx) error {
+			bucket := tx.Bucket([]byte("xchacha"))
+			if bucket == nil {
+				return nil
+			}
+
+			return bucket.Put([]byte("data"), xchachaBIN)
+		})
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	result.Aead, err = chacha20poly1305.NewX(result.Key)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate aead: %v", err)
+	}
+
+	result.AdditionalData = additionalData
+
+	return &result, nil
+}
 
 func setupInitialData(db *bbolt.DB) {
 	persons := []Person{
--- xchacha.go
+++ xchacha.go
@@ -3,15 +3,11 @@ package main
 import (
 	"bytes"
 	"crypto/cipher"
-	"crypto/rand"
 	"crypto/sha256"
 	"encoding/gob"
 	"encoding/hex"
 	"errors"
 	"fmt"
-
-	"go.etcd.io/bbolt"
-	"golang.org/x/crypto/chacha20poly1305"
 )
 
 type Xchacha struct {
@@ -26,89 +22,40 @@ type Encryptor interface {
 	Decrypt(cipherText []byte) (string, error)
 }
 
-var (
-	passphrase     = string("my passsphrase")
-	additionalData = []byte("padding data")
-)
-
-func SetupXchacha() (*Xchacha, error) {
-	var err error
-	var result Xchacha
-
-	switch {
-	case KeyExists("xchacha", []byte("data")):
-		err = db.View(func(tx *bbolt.Tx) error {
-			bucket := tx.Bucket([]byte("xchacha"))
-			if bucket == nil {
-				return nil
-			}
-
-			keyData := bucket.Get([]byte("data"))
-			if keyData == nil {
-				return nil
-			}
-
-			result, err = deserialize(keyData)
-			if err != nil {
-				return fmt.Errorf("failed to deserialize: %v", err)
-			}
-
-			return nil
-		})
-
-		enteredKey := generateKeyFromString(passphrase)
-		if !bytes.Equal(enteredKey, result.Key) {
-			result = Xchacha{}
-			return nil, errors.New("wrong passphrase")
-		}
-
-		if err != nil {
-			return nil, err
-		}
-
-	default:
-		// result.Key = make([]byte, chacha20poly1305.KeySize)
-		// if _, err := rand.Read(result.Key); err != nil {
-		// 	return nil, fmt.Errorf("failed to generate key: %v", err)
-		// }
-		result.Key = generateKeyFromString(passphrase)
-		result.Nonce = make([]byte, chacha20poly1305.NonceSizeX)
-		if _, err := rand.Read(result.Nonce); err != nil {
-			return nil, fmt.Errorf("failed to generate nonce: %v", err)
-		}
-
-		xchachaBIN, err := serialize(result)
-		if err != nil {
-			return nil, fmt.Errorf("failed to serialize: %v", err)
-		}
-
-		err = db.Update(func(tx *bbolt.Tx) error {
-			bucket := tx.Bucket([]byte("xchacha"))
-			if bucket == nil {
-				return nil
-			}
-
-			return bucket.Put([]byte("data"), xchachaBIN)
-		})
-
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	result.Aead, err = chacha20poly1305.NewX(result.Key)
-	if err != nil {
-		return nil, fmt.Errorf("failed to generate aead: %v", err)
-	}
-
-	result.AdditionalData = additionalData
-
-	return &result, nil
-}
-
 func generateKeyFromString(input string) []byte {
 	hash := sha256.Sum256([]byte(input))
 	return hash[:]
+}
+
+func Serialize(data Xchacha) ([]byte, error) {
+	var buffer bytes.Buffer
+	encoder := gob.NewEncoder(&buffer)
+	err := encoder.Encode(data)
+	if err != nil {
+		return nil, err
+	}
+
+	result := []byte(hex.EncodeToString(buffer.Bytes()))
+	return result, nil
+}
+
+func Deserialize(data []byte) (Xchacha, error) {
+	var result Xchacha
+
+	decodedData, err := hex.DecodeString(string(data))
+	if err != nil {
+		return Xchacha{}, err
+	}
+
+	var buffer bytes.Buffer
+	buffer.Write(decodedData)
+
+	decoder := gob.NewDecoder(&buffer)
+	err = decoder.Decode(&result)
+	if err != nil {
+		return Xchacha{}, err
+	}
+	return result, nil
 }
 
 func (x *Xchacha) Encrypt(data string) (string, error) {
@@ -139,35 +86,4 @@ func (x *Xchacha) Decrypt(cipherText []byte) (string, error) {
 	}
 
 	return string(decrypted), nil
-}
-
-func serialize(data Xchacha) ([]byte, error) {
-	var buffer bytes.Buffer
-	encoder := gob.NewEncoder(&buffer)
-	err := encoder.Encode(data)
-	if err != nil {
-		return nil, err
-	}
-
-	result := []byte(hex.EncodeToString(buffer.Bytes()))
-	return result, nil
-}
-
-func deserialize(data []byte) (Xchacha, error) {
-	var result Xchacha
-
-	decodedData, err := hex.DecodeString(string(data))
-	if err != nil {
-		return Xchacha{}, err
-	}
-
-	var buffer bytes.Buffer
-	buffer.Write(decodedData)
-
-	decoder := gob.NewDecoder(&buffer)
-	err = decoder.Decode(&result)
-	if err != nil {
-		return Xchacha{}, err
-	}
-	return result, nil
 }
